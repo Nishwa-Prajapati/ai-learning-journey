799e541d31b370f2506f17913bff26f6f1acf849
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-04 15:31:48 -0400

    learned python comprehensions

--- phase1-foundations/week01-python-oop/day04_comprehensions.py
+++ phase1-foundations/week01-python-oop/day04_comprehensions.py
@@ -1,8 +1,21 @@
+"""
+Day 04 — Comprehensions
+Phase 1, Week 1 | AI Learning Journey
+Date: 26 March 2026
+
+What I learned:
+- Lists of different comprehensions and how they are diffeent from eachother
+- List comprehension, Set comprehension, Dictionary comprehension, Generator comprehension
+- Its a short and clear way to form new sequences using existing iterables. 
+
+"""
+
+
 #List comprehensions
 
 fruits = ['apple','mango','grapes']
 
-#e[xpression for item in iterable if condition]
+#[expression for item in iterable if condition]
 res = [fruits for fruit in fruits if fruits[1]== 'mango']
 print(res)
 res2 = [fruits for fruit in fruits if fruits[1]== 'kiwi']
@@ -14,13 +27,14 @@ no = [num for num in a if num % 2 == 0]
 print(no)
 
 #dictionary comprehension 
+#{key_expression : value_expression for item in iterable if condition}
 states = ['Uttarpradesh','Gujarat','punjab','Maharashtra']
 capitals = ['New delhi', 'Gandhinagar','Chandigarh','Mumbai']
 
 res3 = {state : capital for state, capital in zip(states,capitals)}
 print(res3)
 
-#Generator comprehsion 
+#Generator comprehension 
 res4 = (num for num in range(100) if num % 2 == 0)
 print(list(res4))
 # for num in res4:
