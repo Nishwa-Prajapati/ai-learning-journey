f88f8a68fece6098d07bd6563018141aa00ffa36
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-07 16:26:33 -0400

    logical operators

---
+++ phase1-foundations/week01-python-oop/day05_logicaloperators.py
@@ -0,0 +1,19 @@
+"""
+Day 05 — Arithmetic Operators
+Phase 1, Week 1 | AI Learning Journey
+Date: 7 April 2026
+
+What I learned:
+- And (&&), or (||),  NOT (!=)
+
+"""
+
+x = 23
+y = 2
+z = 2
+
+print((x==y) or (y==z)) #either one should be tru to return true 
+print((x==y) and (y==z)) #  both has to be true to return true 
+print(not(x==z))
+print(not(y==z))
+
