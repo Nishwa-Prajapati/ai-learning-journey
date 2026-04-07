eaef5c21eefff07d582b469656b9d9dca9a6a3b9
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-07 16:24:26 -0400

    Arithmetic operators

---
+++ phase1-foundations/week01-python-oop/day05_arithmeticoperators.py
@@ -0,0 +1,25 @@
+"""
+Day 05 — Arithmetic Operators
+Phase 1, Week 1 | AI Learning Journey
+Date: 7 April 2026
+
+What I learned:
+- Recalling the basics ( +, -, *, /, //, **, %)
+
+"""
+
+
+x = 50
+y = 5
+
+print("Addition :", x+y)
+print("Subtraction :", x-y)
+print("Multiplicatio :", x*y)
+print("Division :", x/y)
+print("Float division :", 34/3)
+
+#Advance operators
+
+print("Integer Division  :", x//y)
+print("Exponential :", x**y)
+print("Modulo :", x%y)
