18ee0fd76c2ae566ece835d03d0c46574318d10f
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-07 16:25:50 -0400

    assignments operators

---
+++ phase1-foundations/week01-python-oop/day05_assignmentoperators.py
@@ -0,0 +1,29 @@
+"""
+Day 05 — Arithmetic Operators
+Phase 1, Week 1 | AI Learning Journey
+Date: 7 April 2026
+
+What I learned:
+- +=. -=, *=, /=, **= and etc
+
+"""
+
+x = 88
+x += 2
+print(x)
+
+y = 45
+y *= 3
+print(y)
+
+z = 33
+z -= 10
+print(z)
+
+a = 25
+a /= 2
+print(a)
+
+b = 123
+b **= 2
+print(b)
