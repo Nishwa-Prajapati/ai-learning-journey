0c3ea34233a3815d124f114887a4fb6a2311fb22
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-07 16:25:14 -0400

    comparison operators

---
+++ phase1-foundations/week01-python-oop/day05_comparisonoperator.py
@@ -0,0 +1,34 @@
+"""
+Day 05 — Arithmetic Operators
+Phase 1, Week 1 | AI Learning Journey
+Date: 7 April 2026
+
+What I learned:
+-   > , <, >=, <=, ==
+
+"""
+
+x = 10
+y = 5
+a = 10
+b = 34
+
+print(x>y)
+print(y>x)
+
+print(x>=y)
+print(x>=a)
+print(y>=x)
+
+print(x<y)
+print(x<b)
+
+print(x<=b)
+print(x<=a)
+print(x<=y)
+
+print(x!=y)
+print(x!=a)
+
+
+
