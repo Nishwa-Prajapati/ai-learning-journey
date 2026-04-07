b47954d98a2f3a780a52de19183e5afbe93e269c
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-07 16:27:14 -0400

    membership operator

---
+++ phase1-foundations/week01-python-oop/day05_membershipoperator.py
@@ -0,0 +1,15 @@
+"""
+Day 05 — Arithmetic Operators
+Phase 1, Week 1 | AI Learning Journey
+Date: 7 April 2026
+
+What I learned:
+- recalling basics. (in and not in)
+
+"""
+
+my_string = " The quick brown fox jump over the lazy dog"
+
+print("fox" in my_string)
+print("Apple" in my_string)
+print("Apple" not in my_string)
