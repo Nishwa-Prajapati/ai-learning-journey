d8c894d5d9307ad951737d5434688a7189e41bd7
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-09 19:00:18 -0400

    length and indexing

---
+++ phase1-foundations/week01-python-oop/day06_lenandindex.py
@@ -0,0 +1,17 @@
+"""
+Day 05 — Length and index
+Phase 1, Week 1 | AI Learning Journey
+Date: 9 April 2026
+
+What I learned:
+- recalling basics.
+
+"""
+list1 = [23,56,1,89,333,5634.98]
+print("Length of thw given list : ",len(list1))
+
+print("Index of 1 in the list is : ", list1.index(1))
+
+set1 = {2,3,4,5}
+print(len(set1))
+# print(set1.index(2))   -> set has no attribute index
