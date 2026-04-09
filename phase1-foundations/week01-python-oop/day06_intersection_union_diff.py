97d3c92c378d17cdb372010c0ec24aa0f777a824
Nishwa Prajapati <nishwa.prajapati@gmail.com>
2026-04-09 17:39:05 -0400

    Learned set intersection,union,difference

---
+++ phase1-foundations/week01-python-oop/day06_intersection_union_diff.py
@@ -0,0 +1,16 @@
+"""
+Day 05 — Set union, intersection, difference
+Phase 1, Week 1 | AI Learning Journey
+Date: 9 April 2026
+
+What I learned:
+- recalling basics. (union (|), intersection (&), difference(-) )
+
+"""
+
+x = {34, 2, 6 ,7, 1}
+y = {0,5,2,5,7,22,66,34}
+
+print("Union of x & y :", (x | y))
+print("Intersection of set x & y : ", (x & y))
+print("Difference of set x & y : ", (x - y))
